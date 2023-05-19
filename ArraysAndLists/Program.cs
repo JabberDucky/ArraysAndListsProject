9448546962fc912b2cf170066f60587ef60d530e
Katherine Vail <jabberduckygame@gmail.com>
2023-05-19 13:43:41 -0500

    removed code

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -32,9 +32,8 @@ namespace ArraysAndLists
 
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var evens = new List<int>();
-            evens.Add(0);
             var odds = new List<int>();
-            odds.Add(0);   
+          
 
             foreach (int number in numbers)
             {
